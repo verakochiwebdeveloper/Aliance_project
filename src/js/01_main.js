85786981ffa6878295b57f14ce114ccbb6488c62
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-03-21 11:04:25 +0300

    mobilefix

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,6 +1,21 @@
-const scrollWrapper = document.querySelector (".header__wrapper"),
-  mobileMenuButton = document.querySelector (".burger-btn"),
-  mobileMenu = document.querySelector (".header__wrapper-mobile")
+const scrollWrapper = document.querySelector (".header__wrapper");
+const  mobileMenuButton = document.querySelector (".burger-btn");
+const mobileMenu = document.querySelector (".header__wrapper-mobile");
+
+const openMenu = (event) => {
+    mobileMenu.classList.add ("--opene-mobile-menu")
+    document.body.style.overflow= "hidden";
+    scrollWrapper.classList.add("--header__wrapper-white");
+    mobileMenuButton.classList.add("--opene-mobile-menu")
+}
+
+const closeMenu = (event) => {
+    mobileMenu.classList.remove ("--opene-mobile-menu")
+    document.body.style.overflow= "";
+    scrollWrapper.classList.remove("--header__wrapper-white")
+    mobileMenuButton.classList.add("--opene-mobile-menu")
+}
+
 
 window.addEventListener('scroll', ()=> {
     
@@ -29,7 +44,13 @@ window.addEventListener('scroll', ()=> {
  */
 
  
-    mobileMenu.addEventListener('clck',(event)=> {
+    mobileMenuButton.addEventListener('clck',(event)=> {
         event.preventDefault();
-        mobileMenu.classList.toggle('show')
+        mobileMenu.classList.toggle("--opene-mobile-menu")
+        if (mobileMenu.classList.contains())
+        closeMenu();
+         else {
+            openMenu()
+
+         }
     })
